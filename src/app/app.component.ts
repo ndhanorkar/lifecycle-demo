ecd04943c2ceafdfa6d3bf7556aab29dc5cd4fc7
nirav dhanorkar <nirav dhanorkar>
2018-12-17 00:36:56 +0530

    comp changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { 
-  Component
+  Component,
+  OnInit,  
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
 } from '@angular/core';
 
 @Component({
@@ -7,8 +15,34 @@ import {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent
+export class AppComponent implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked
 {
   name: string = ""
+
+
+  ngOnInit(){
+    console.log("parent Init")
+  }
+  ngOnChanges(){
+    console.log("parent Changes")
+  }
+  ngDoCheck(){
+    console.log("parent DoCheck")
+  }
+  ngAfterContentInit(){
+    console.log("parent AfterContentInit")
+  }
+  ngAfterContentChecked(){
+    console.log("parent AfterContentChecked")
+  }
+  ngAfterViewInit(){
+    console.log("parent AfterViewInit")
+  }
+  ngAfterViewChecked(){
+    console.log("parent AfterViewChecked")
+  }
+  ngOnDestroy(){
+    console.log("parent Destroy")
+  }
 
 }
